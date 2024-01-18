f836aaebba1e19bd6dcfb7c30bde9915e5c56083
Samosadov <for-spam2@yandex.ru>
2024-01-18 15:18:38 +0300

    Minimal time - check

--- src/Answear.java
+++ src/Answear.java
@@ -1,15 +1,47 @@
-import java.util.Date;
+import java.util.TreeSet;
+import java.util.concurrent.TimeUnit;
 
 public class Answear {
-    private Date minTime;
+    private long minTime;
     private int difPrice;
+    private float avgPrice;
+    private float median;
+    private TreeSet<Integer> prices = new TreeSet<>();
 
-    public Date getMinTime() {
+    public Answear(long minTime, int price) {
+        this.minTime = minTime;
+        prices.add(price);
+        avgPrice = 0.0F;
+        median = 0.0F;
+    }
+
+    public float getAvgPrice() {
+        return avgPrice;
+    }
+
+    public void setAvgPrice(int price) {
+        prices.add(price);
+        for (Integer i : prices) {
+            avgPrice += i;
+        }
+        avgPrice /= prices.size();
+//        median = (prices.size() % 2 == 1) ? prices. :;   Вычислить медиану
+    }
+
+    public float getMedian() {
+        return median;
+    }
+
+    public void setMedian(int median) {
+        this.median = median;
+    }
+
+    public long getMinTime() {
         return minTime;
     }
 
     public void setMinTime(long minTime) {
-        this.minTime = new Date(minTime);
+        this.minTime = minTime;
     }
 
     public int getDifPrice() {
@@ -20,15 +52,11 @@ public class Answear {
         this.difPrice = difPrice;
     }
 
-    public Answear(long minTime, int difPrice) {
-        this.minTime = new Date(minTime);
-        this.difPrice = difPrice;
-    }
-
     @Override
     public String toString() {
-        return "Минимальное время: " + minTime +
-                "\nРазница между средней ценой и медианой: " + difPrice +
-                '}';
+        long mins = TimeUnit.MILLISECONDS.toMinutes(minTime) % 60;
+        long hours = TimeUnit.MILLISECONDS.toHours(minTime);
+        return "Минимальное время перелёта: " + hours + ":" + mins +
+                "\nРазница между средней ценой и медианой: " + difPrice + '\n';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,17 @@ import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-//- Минимальное время полета между городами Владивосток и Тель-Авив для каждого авиаперевозчика
-//- Разницу между средней ценой  и медианой для полета между городами Владивосток и Тель-Авив
 
 public class Main {
+//    private static TreeMap<String, Answear> carriers = new TreeMap<>(Comparator.comparingInt(o -> o.getPrice));
+    private static HashMap<String, Answear> carriers = new HashMap<>();
 
     public static void jsonRead(String fileName){
-
-        HashMap<String, Answear> carriers = new HashMap<>();
         int difPrice = 0;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             Gson gson = new Gson();
             TicketsList ticketsList = gson.fromJson(new JsonReader(reader), TicketsList.class);
-// Отфильтровать перевозки по VVO - TLV, занести в словарь и
             Calendar date1 = new GregorianCalendar();
             Calendar date2 = new GregorianCalendar();
             for (Ticket ticket: ticketsList.tickets) {
@@ -28,21 +25,36 @@ public class Main {
                         parse(ticket.arrival_date + " " + ticket.arrival_time));
                 long millis = date2.getTimeInMillis() - date1.getTimeInMillis();
 
-                if (carriers.containsKey(ticket.carrier)) {
-                    carriers.put(ticket.carrier, new Answear(millis, difPrice));
-                }
+//                TreeMap<Person, Integer> map = new TreeMap<>(Comparator.comparingInt(o -> o.age));
 
-                System.out.println(ticket.carrier + " " + millis);
+                if (!carriers.containsKey(ticket.carrier)) {
+                        if (ticket.origin.equals("VVO") && ticket.destination.equals("TLV")) {
+                            carriers.put(ticket.carrier, new Answear(millis, ticket.price));
+//                            carriers.get(ticket.carrier).setAvgPrice(ticket.price);
+                        }
+                }
+                else {
+                    if ((ticket.origin.equals("VVO") && ticket.destination.equals("TLV"))
+                            && millis < carriers.get(ticket.carrier).getMinTime()) {
+                        carriers.get(ticket.carrier).setMinTime(millis);
+                        carriers.get(ticket.carrier).setAvgPrice(ticket.price);
+                    }
+                }
             }
 
-        } catch (IOException | ParseException e) {
+        } catch (IOException e) {
             System.out.println("** Ошибка! ** Файл не найден.");
+        } catch (ParseException e) {
+            System.out.println("** Ошибка! ** Не могу прочитать файл.");
         }
     }
 
     public static void taskWriter(String fileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-
+            for (String key : carriers.keySet()) {
+                writer.write("Перевозчик: " + key + '\n' + carriers.get(key));
+            }
+            writer.flush();
 
         } catch (IOException e) {
             System.out.println("** Ошибка! ** Файл не создан.");
